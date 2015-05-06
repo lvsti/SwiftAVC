9473c07e59e296152673813153e9db138e9609ea
Lk Xf <elveestei@gmail.com>
2015-05-06 21:56:03 +0200

    Initial Commit

---
+++ SwiftAVC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftAVC
+//
+//  Created by Tamas Lustyik on 2015.05.06..
+//  Copyright (c) 2015 Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
