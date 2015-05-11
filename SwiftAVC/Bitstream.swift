10c9c3f05a50d8e43f5a64b14dbb4797524939dd
Lk Xf <elveestei@gmail.com>
2015-05-12 00:03:19 +0200

    Raw bitstream parser

---
+++ SwiftAVC/Bitstream.swift
@@ -0,0 +1,57 @@
+//
+//  Bitstream.swift
+//  SwiftAVC
+//
+//  Created by Tamas Lustyik on 2015.05.06..
+//  Copyright (c) 2015 Tamas Lustyik. All rights reserved.
+//
+
+import Foundation
+
+
+let kLeadBitmask: [UInt8] = [0b11111111, 0b01111111, 0b00111111, 0b00011111, 0b00001111, 0b00000111, 0b00000011, 0b00000001]
+
+
+struct Bitstream {
+    private let _data: NSData
+    let length: Int
+
+    init(data: NSData) {
+        _data = data
+        length = _data.length * 8
+    }
+    
+    func read(offset: Int, count: Int) -> Int? {
+        assert(count <= sizeof(Int)*8)
+        
+        let startIdx = offset / 8
+        let endIdx = (offset + count) / 8
+        
+        if endIdx >= _data.length {
+            return nil
+        }
+        
+        let ptr = UnsafePointer<UInt8>(_data.bytes)
+        var value: Int = Int(ptr[startIdx] & kLeadBitmask[offset % 8])
+        
+        if endIdx > startIdx + 1 {
+            for i in startIdx+1..<endIdx {
+                value <<= 8
+                value |= Int(ptr[i])
+            }
+        }
+        
+        let newBitOffset = (offset + count) % 8
+        if newBitOffset > 0 {
+            if startIdx == endIdx {
+                value >>= 8 - newBitOffset
+            } else {
+                value <<= Int(newBitOffset)
+                value |= Int(ptr[endIdx] >> (8 - newBitOffset))
+            }
+        }
+        
+        return value
+    }
+}
+
